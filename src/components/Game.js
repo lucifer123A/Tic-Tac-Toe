a13dad27e194eae8f5c01362ebda0e92ccdbb50a
Lokesh Dulani <lokeshdulani91@gmail.com>
2020-05-31 23:44:04 -0700

    basic Tic Tac Toe is ready

---
+++ src/components/Game.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Board from './Board';
+
+class Game extends React.Component{
+    render(){
+        return(
+            <div className='game'>
+                <div className='game-board'>
+                    <Board/>
+                </div>
+                <div className='game-info'>
+                    <div>{/*status*/}</div>
+                    <ol>{/*TO DO*/}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game
